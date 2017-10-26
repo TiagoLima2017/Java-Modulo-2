273bdaa38baf942b35f25b7026fd95b8926baaa0
TiagoLima2017 <32169068+TiagoLima2017@users.noreply.github.com>
2017-10-26 17:23:57 +0100

    Create Leia1.java

---
+++ Leia1.java
@@ -0,0 +1,11 @@
+package pacote1;
+import java.io.*;
+public class leia {
+
+	public static void main(String[] args) throws IOException
+		{String C = "";
+		System.out.println("Digite um nº");
+		InputStreamReader LerS = new InputStreamReader (System.in);
+		BufferedReader GuardaS = new BufferedReader (LerS);
+		C = GuardaS.readLine();
+		System.out.printf("Digitou: %d", Integer.valueOf(C));}}
